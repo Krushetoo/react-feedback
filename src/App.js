8b3b896ad9fcb7b5e4f3fc11708a8422aab8d45c
Krusheto <solostrikebt@hotmail.com>
2022-03-02 14:34:58 +0100

    test 2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
-import FeedbackList from "./components/FeedbackList";
 import FeedbackStats from "./components/FeedbackStats";
+import FeedbackList from "./components/FeedbackList";
 import FeedbackForm from "./components/FeedbackForm";
 import AboutPage from "./pages/AboutPage";
 import AboutIconLink from "./components/AboutIconLink";
